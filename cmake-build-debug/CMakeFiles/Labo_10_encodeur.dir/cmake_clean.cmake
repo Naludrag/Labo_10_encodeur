c97228e2c465d7c53cf488fb223ba3c42e053170
BecaudA <becaud.arthur@gmail.com>
2019-01-08 13:25:26 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Labo_10_encodeur.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Labo_10_encodeur.dir/main.cpp.obj"
+  "Labo_10_encodeur.pdb"
+  "Labo_10_encodeur.exe"
+  "Labo_10_encodeur.exe.manifest"
+  "libLabo_10_encodeur.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Labo_10_encodeur.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Labo_10_encodeur.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Labo_10_encodeur.
+# This may be replaced when dependencies are built.
